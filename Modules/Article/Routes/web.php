a63fd77e16bb52e07dcdeb653ee68275a65f3ee2
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-07 15:31:52 +0600

    Added routes for Article modules

--- Modules/Article/Routes/web.php
+++ Modules/Article/Routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::prefix('article')->group(function() {
-    Route::get('/', 'ArticleController@index');
+use Modules\Article\Http\Controllers\CategoriesController;
+use Modules\Article\Http\Controllers\PagesController;
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    /**
+     * Category Management Routes
+     */
+    Route::group(['prefix' => ''], function () {
+        Route::resource('categories', CategoriesController::class);
+        Route::get('categories/trashed/view', [CategoriesController::class, 'trashed'])->name('categories.trashed');
+        Route::delete('categories/trashed/destroy/{id}', [CategoriesController::class, 'destroyTrash'])->name('categories.trashed.destroy');
+        Route::put('categories/trashed/revert/{id}', [CategoriesController::class, 'revertFromTrash'])->name('categories.trashed.revert');
+    });
+
+    /**
+     * Page Management Routes
+     */
+    Route::group(['prefix' => ''], function () {
+        Route::resource('pages', PagesController::class);
+        Route::get('pages/trashed/view', [PagesController::class, 'trashed'])->name('pages.trashed');
+        Route::delete('pages/trashed/destroy/{id}', [PagesController::class, 'destroyTrash'])->name('pages.trashed.destroy');
+        Route::put('pages/trashed/revert/{id}', [PagesController::class, 'revertFromTrash'])->name('pages.trashed.revert');
+    });
 });
