fdb12d82ede8ffe98c94aec18cc2a646e97c570d
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-07 18:49:13 +0600

    Updated frontend links

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -9,18 +9,29 @@
 <main class="main">
 
   <!-- Page Content -->
-
   <div class="main-header">
     <div class="container">
-        <h1 class="display-3">Welcome to Lara CMS</h1>
+        <h1 class="display-4">Welcome to Lara CMS</h1>
         <p class="lead">
-            Admin Panel with Laravel
+            A Great Customized admin panel built with Laravel...
         </p>
-        <a href="{{ route('admin.index') }}" class="btn btn-primary btn-lg">Just Go Now ...</a>
+        <a href="{{ route('admin.index') }}" class="btn btn-primary btn-lg">Go ››</a>
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="display-4">Get Frontend Themes</h1>
+        <div class="row justify-content-center">
+            <div class="card card-body col-sm-12 col-md-3 col-lg-4">
+                <h4 class="mb-3">Business Website</h4>
+                <a href="{{ route('demo.business.index') }}" class="btn btn-info">
+                    Go ››
+                </a>
+            </div>
+        </div>
+
     </div>
     <!-- /.container -->
   </div>
-  <!-- /.main-header -->
 
     <!-- Contact Form -->
     <div class="contact-form" id="contact">
@@ -50,8 +61,6 @@
       </form>
     </div>
     <!-- /.contact-form -->
-
-
 </main>
 
 @endsection
