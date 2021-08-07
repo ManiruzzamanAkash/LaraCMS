fd11c36388f87b20e2d73f553f2b568b3a2d184b
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-07 18:50:39 +0600

    Delete some links from front page

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -7,16 +7,8 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="{{ route('index') }}">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('admin.index') }}">Admin Panel</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#contact">Contact</a>
           </li>
         </ul>
       </div>
