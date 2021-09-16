193a4dd14057346633d21b0df3a9efc3df3b4d02
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-16 07:19:56 +0600

    Designed Book a Service Modal

--- Modules/Booking/Resources/views/frontend/partials/booking-request-form.blade.php
+++ Modules/Booking/Resources/views/frontend/partials/booking-request-form.blade.php
@@ -1,5 +1,4 @@
-<form class="contact-form custom-form-style-1" method="POST"
-    novalidate="novalidate">
+<form class="contact-form custom-form-style-1" method="POST" data-parsley-validate>
     <div class="contact-form-success alert alert-success d-none mt-4">
         <strong>Success!</strong> Your request has been sent to us.
     </div>
@@ -10,19 +9,38 @@
     </div>
 
     <div class="row">
-        <div class="form-group col-sm-12 col-md-6 pb-1 mb-3">
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
+            <label for="name">Your Name</label>
             <input type="text" value="" data-msg-required="Please enter your name." maxlength="100"
-                class="form-control" name="name" placeholder="Your Name" required="">
+                class="form-control" id="name" name="name" placeholder="Your Name" required>
         </div>
-        <div class="form-group col-sm-12 col-md-6 pb-1 mb-3">
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
+            <label for="phone_no">Your Phone Number</label>
             <input type="text" value="" data-msg-required="Please enter your phone number." maxlength="100"
-                class="form-control" name="phone" placeholder="Your Phone Number" required="">
+                class="form-control" id="phone_no" name="phone_no" placeholder="Your Phone Number" required>
+        </div>
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
+            <label for="email">Your Email Address</label>
+            <input type="email" value="" data-msg-required="Please enter your email address" maxlength="100"
+                class="form-control" id="email" name="email" placeholder="Your Email Address" required>
         </div>
     </div>
     <div class="row">
-        <div class="form-group col pb-1 mb-3">
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
             <div class="custom-select-1">
-                <select data-msg-required="Please select a service." class="form-control" name="service" required="">
+                <label for="service_category">Service Category</label>
+                <select data-msg-required="Please select a service." class="form-control" name="service_category" required>
+                    <option value="" selected="">Select Service Category</option>
+                    <option value="Build Services">Building Services</option>
+                    <option value="Post Construction">Post Construction</option>
+                    <option value="Office Cleaning">Office Cleaning</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
+            <div class="custom-select-1">
+                <label for="service">Service</label>
+                <select data-msg-required="Please select a service." class="form-control" name="service" required>
                     <option value="" selected="">Select Service</option>
                     <option value="Build Services">Building Services</option>
                     <option value="Post Construction">Post Construction</option>
@@ -30,15 +48,31 @@
                 </select>
             </div>
         </div>
+
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
+            <label for="date">Booking Date</label>
+            <input type="date" value="" data-msg-required="Please enter date"
+                class="form-control" id="date" name="date" placeholder="Enter date and time" required>
+        </div>
+
+        <div class="form-group col-sm-12 col-md-4 pb-1 mb-3">
+            <label for="start_time">Start at</label>
+            <input type="time" value="" data-msg-required="Please enter start time"
+                class="form-control" id="start_time" name="start_time" placeholder="Enter start time" required>
+        </div>
+
+        <div class="form-group col-sm-12 col-md-8 pb-1 mb-3">
+            <label for="message">Message</label>
+            <textarea data-msg-required="Please enter your message (if any)"
+                class="form-control" id="message" name="message" placeholder="Enter your message (if any)"></textarea>
+        </div>
     </div>
     <div class="row">
         <div class="form-group col">
-            <button type="submit" class="btn btn-primary btn-modern font-weight-bold text-3 px-5 py-3"
-                data-loading-text="Loading...">
+            <button type="submit" class="btn btn-primary btn-modern font-weight-bold text-3 px-5 py-3" data-loading-text="Loading...">
                 SEND REQUEST
             </button>
-            <button type="button" class="btn btn-secondary btn-modern font-weight-bold text-3 px-5 py-3"
-                data-bs-dismiss="modal">
+            <button type="button" class="btn btn-secondary btn-modern font-weight-bold text-3 px-5 py-3" data-bs-dismiss="modal">
                 CANCEL
             </button>
         </div>
