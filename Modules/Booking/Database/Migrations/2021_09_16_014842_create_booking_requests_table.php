ffe69f33d4e8e84b2bbe0b4830af6369a325251a
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-16 19:30:42 +0600

    Added booking request migration

---
+++ Modules/Booking/Database/Migrations/2021_09_16_014842_create_booking_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('phone_no', 20);
+            $table->string('email', 100);
+            $table->unsignedBigInteger('service_id')->index();
+            $table->unsignedBigInteger('service_category_id')->index();
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->text('message')->nullable();
+            $table->string('status')->index()->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking_requests');
+    }
+}
