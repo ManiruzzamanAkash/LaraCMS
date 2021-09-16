4aeae7d2d2571b9554cf66ea283ea63cfade5bf8
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-16 19:35:15 +0600

    Changes header phone numbers

--- Modules/ThemeBusiness/Resources/views/frontend/layouts/partials/header.blade.php
+++ Modules/ThemeBusiness/Resources/views/frontend/layouts/partials/header.blade.php
@@ -37,7 +37,7 @@
                             </div>
                             <div class="feature-box-info ps-2">
                                 <p class="font-weight-semibold line-height-1 text-2 pb-0 mb-1">CALL US NOW</p>
-                                <a href="tel:+1234567890" class="text-color-tertiary text-color-hover-primary text-decoration-none font-weight-bold line-height-1 custom-font-size-1 pb-0">800-123-4567</a>
+                                <a href="tel:+61297937174" class="text-color-tertiary text-color-hover-primary text-decoration-none font-weight-bold line-height-1 custom-font-size-1 pb-0">+61 297937174</a>
                             </div>
                         </div>
                         <button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
