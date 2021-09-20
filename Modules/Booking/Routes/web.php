76819e9ab7dbba7b1a01256d7ba3390f9f152ae2
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-20 16:08:13 +0600

    Added booking request routes

--- Modules/Booking/Routes/web.php
+++ Modules/Booking/Routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
+use Modules\Booking\Http\Controllers\BookingRequestController;
+
 Route::prefix('booking-request')->group(function () {
-    Route::post('store', 'BookingRequestController@store')->name('booking.request.store');
+    Route::post('store', [BookingRequestController::class, 'store'])->name('booking.request.store');
+});
+
+Route::prefix('admin/booking-request')->group(function () {
+    Route::get('', [BookingRequestController::class, 'index'])->name('admin.booking_request.index');
+    Route::get('{id}', [BookingRequestController::class, 'edit'])->name('admin.booking_request.edit');
+    Route::put('{id}', [BookingRequestController::class, 'update'])->name('admin.booking_request.update');
+    Route::delete('{id}', [BookingRequestController::class, 'destroy'])->name('admin.booking_request.delete');
+});
+
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::group(['prefix' => 'booking-request', 'as' => 'booking_request.'], function () {
+        Route::get('', [BookingRequestController::class, 'index'])->name('index');
+        Route::get('{id}', [BookingRequestController::class, 'edit'])->name('edit');
+        Route::put('{id}', [BookingRequestController::class, 'update'])->name('update');
+        Route::delete('{id}', [BookingRequestController::class, 'destroy'])->name('delete');
+
+        Route::get('trashed/view', [BookingRequestController::class, 'trashed'])->name('trashed');
+        Route::delete('trashed/destroy/{id}', [BookingRequestController::class, 'destroyTrash'])->name('trashed.destroy');
+        Route::put('trashed/revert/{id}', [BookingRequestController::class, 'revertFromTrash'])->name('trashed.revert');
+    });
 });
