a7c68e5e99065323ac1bbc618eff8266a2346d24
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-20 16:11:21 +0600

    Added booking request page and other controller methods for booking request

--- Modules/Booking/Http/Controllers/BookingRequestController.php
+++ Modules/Booking/Http/Controllers/BookingRequestController.php
@@ -2,13 +2,144 @@
 
 namespace Modules\Booking\Http\Controllers;
 
+use Carbon\Carbon;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Booking\Entities\BookingRequest;
 use Modules\Booking\Http\Requests\BookingRequest as BookingFormRequest;
+use Yajra\DataTables\Facades\DataTables;
 
 class BookingRequestController extends Controller
 {
+    public $user;
+
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            $this->user = auth()->user();
+            return $next($request);
+        });
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($isTrashed = false)
+    {
+
+        if (is_null($this->user) || !$this->user->can('booking_request.view')) {
+            $message = 'You are not allowed to access this page !';
+            return view('errors.403', compact('message'));
+        }
+
+        if (request()->ajax()) {
+            $status = empty(request()->status) ? 'pending' : request()->status;
+
+            if ($isTrashed) {
+                $booking_requests = BookingRequest::orderBy('id', 'desc')
+                    ->where('status', $status)
+                    ->get();
+            } else {
+                $booking_requests = BookingRequest::orderBy('id', 'desc')
+                    ->where('status', $status)
+                    ->get();
+            }
+
+            $datatable = DataTables::of($booking_requests, $isTrashed)
+                ->addIndexColumn()
+                ->addColumn(
+                    'action',
+                    function ($row) use ($isTrashed) {
+                        $csrf = "" . csrf_field() . "";
+                        $method_delete = "" . method_field("delete") . "";
+                        $method_put = "" . method_field("put") . "";
+                        $html = "";
+
+                        $deleteRoute =  route('admin.booking_request.delete', [$row->id]);
+                        if ($this->user->can('booking_request.edit')) {
+                            $html = '<a class="btn waves-effect waves-light btn-success btn-sm btn-circle" title="View & Edit Request Details" href="' . route('admin.booking_request.edit', $row->id) . '"><i class="fa fa-eye"></i></a>';
+                        }
+
+                        if ($this->user->can('booking_request.delete')) {
+                            $html .= '<a class="btn waves-effect waves-light btn-danger btn-sm btn-circle ml-2 text-white" title="Delete Admin" id="deleteItem' . $row->id . '"><i class="fa fa-trash"></i></a>';
+                        }
+
+                        $html .= '<script>
+                            $("#deleteItem' . $row->id . '").click(function(){
+                                swal.fire({ title: "Are you sure?",text: "Request will be deleted as trashed !",type: "warning",showCancelButton: true,confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, delete it!"
+                                }).then((result) => { if (result.value) {$("#deleteForm' . $row->id . '").submit();}})
+                            });
+                        </script>';
+
+                        $html .= '<script>
+                            $("#deleteItemPermanent' . $row->id . '").click(function(){
+                                swal.fire({ title: "Are you sure?",text: "Request will be deleted permanently, both from trash !",type: "warning",showCancelButton: true,confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, delete it!"
+                                }).then((result) => { if (result.value) {$("#deletePermanentForm' . $row->id . '").submit();}})
+                            });
+                        </script>';
+
+                        $html .= '<script>
+                            $("#revertItem' . $row->id . '").click(function(){
+                                swal.fire({ title: "Are you sure?",text: "Request will be revert back from trash !",type: "warning",showCancelButton: true,confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, Revert Back!"
+                                }).then((result) => { if (result.value) {$("#revertForm' . $row->id . '").submit();}})
+                            });
+                        </script>';
+
+                        $html .= '
+                            <form id="deleteForm' . $row->id . '" action="' . $deleteRoute . '" method="post" style="display:none">' . $csrf . $method_delete . '
+                                <button type="submit" class="btn waves-effect waves-light btn-rounded btn-success"><i
+                                        class="fa fa-check"></i> Confirm Delete</button>
+                                <button type="button" class="btn waves-effect waves-light btn-rounded btn-secondary" data-dismiss="modal"><i
+                                        class="fa fa-times"></i> Cancel</button>
+                            </form>';
+
+                        $html .= '
+                            <form id="deletePermanentForm' . $row->id . '" action="' . $deleteRoute . '" method="post" style="display:none">' . $csrf . $method_delete . '
+                                <button type="submit" class="btn waves-effect waves-light btn-rounded btn-success"><i
+                                        class="fa fa-check"></i> Confirm Permanent Delete</button>
+                                <button type="button" class="btn waves-effect waves-light btn-rounded btn-secondary" data-dismiss="modal"><i
+                                        class="fa fa-times"></i> Cancel</button>
+                            </form>';
+                        return $html;
+                    }
+                )
+
+                ->editColumn('status', function ($row) {
+                    $statusText = ucwords($row->status);
+                    if ($row->status === 'pending') {
+                        return '<span class="badge badge-warning font-weight-100">'.$statusText.'</span>';
+                    } else if ($row->status === 'completed') {
+                        return '<span class="badge badge-success">'.$statusText.'</span>';
+                    }  else if ($row->status === 'cancelled') {
+                        return '<span class="badge badge-danger">'.$statusText.'</span>';
+                    } else {
+                        return '<span class="badge badge-info">'.$statusText.'</span>';
+                    }
+                });
+
+            $rawColumns = ['name', 'email', 'phone_no', 'service_name', 'start_date', 'status', 'action'];
+
+            return $datatable->rawColumns($rawColumns)->make(true);
+        }
+
+        $count_booking_requests             = BookingRequest::select('id')->count();
+        $count_pending_booking_requests     = BookingRequest::select('id')->where('status', 'pending')->count();
+        $count_processing_booking_requests  = BookingRequest::select('id')->where('status', 'processing')->count();
+        $count_completed_booking_requests   = BookingRequest::select('id')->where('status', 'completed')->count();
+        $count_cancelled_booking_requests   = BookingRequest::select('id')->where('status', 'cancelled')->count();
+
+        return view('booking::backend.booking_request.index', compact('count_booking_requests', 'count_pending_booking_requests', 'count_processing_booking_requests', 'count_cancelled_booking_requests', 'count_completed_booking_requests'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(BookingFormRequest $request)
     {
         try {
@@ -16,7 +147,115 @@ class BookingRequestController extends Controller
             session()->flash('success', 'Your request has been sent to authority. An agent will communicate with you soon.');
             return back();
         } catch (Exception $e) {
-            session()->flash('sticky_error', $e->getMessage());
+            session()->flash('error', $e->getMessage());
         }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        if (is_null($this->user) || !$this->user->can('booking_request.edit')) {
+            $message = 'You are not allowed to access this page !';
+            return view('errors.403', compact('message'));
+        }
+
+        $booking_request = BookingRequest::find($id);
+
+        return view('booking::backend.booking_request.edit', compact('booking_requests'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if (is_null($this->user) || !$this->user->can('booking_request.edit')) {
+            $message = 'You are not allowed to access this page !';
+            return view('errors.403', compact('message'));
+        }
+
+        $booking_request = BookingRequest::find($id);
+
+        if (is_null($booking_request)) {
+            session()->flash('error', "The page is not found !");
+            return redirect()->route('admin.booking_request.index');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (is_null($this->user) || !$this->user->can('booking_request.delete')) {
+            $message = 'You are not allowed to access this page !';
+            return view('errors.403', compact('message'));
+        }
+
+        $booking_request = BookingRequest::find($id);
+        if (is_null($booking_request)) {
+            session()->flash('error', "The page is not found !");
+            return redirect()->route('admin.booking_request.trashed');
+        }
+
+        $booking_request->status = 'cancelled';
+        $booking_request->deleted_by = auth()->user()->id;
+        $booking_request->status = 0;
+        $booking_request->save();
+
+        session()->flash('success', 'Request has been deleted successfully as trashed !!');
+        return redirect()->route('admin.booking_request.trashed');
+    }
+
+    /**
+     * destroyTrash
+     *
+     * @param integer $id
+     * @return void Destroy the data permanently
+     */
+    public function destroyTrash($id)
+    {
+        if (is_null($this->user) || !$this->user->can('booking_request.delete')) {
+            $message = 'You are not allowed to access this page !';
+            return view('errors.403', compact('message'));
+        }
+        $booking_request = BookingRequest::find($id);
+        if (is_null($booking_request)) {
+            session()->flash('error', "The page is not found !");
+            return redirect()->route('admin.booking_request.trashed');
+        }
+
+        // Delete Category permanently
+        $booking_request->delete();
+
+        session()->flash('success', 'Request has been deleted permanently !!');
+        return redirect()->route('admin.booking_request.trashed');
+    }
+
+    /**
+     * trashed
+     *
+     * @return view the trashed data list -> which data status = 0 and deleted_at != null
+     */
+    public function trashed()
+    {
+        if (is_null($this->user) || !$this->user->can('booking_request.view')) {
+            $message = 'You are not allowed to access this page !';
+            return view('errors.403', compact('message'));
+        }
+
+        return $this->index(true);
     }
 }
---
+++ Modules/Booking/Resources/views/backend/booking_request/index.blade.php
@@ -0,0 +1,54 @@
+@extends('backend.layouts.master')
+
+@section('title')
+    @include('booking::backend.booking_request.partials.title')
+@endsection
+
+@section('admin-content')
+    @include('booking::backend.booking_request.partials.header-breadcrumbs')
+    <div class="container-fluid">
+        @include('booking::backend.booking_request.partials.top-show')
+        @include('backend.layouts.partials.messages')
+        <div class="table-responsive product-table">
+            <table class="table table-striped table-bordered display ajax_view" id="booking_requests_table">
+                <thead>
+                    <tr>
+                        <th>Sl</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Service Name</th>
+                        <th>Service Request Date/Time</th>
+                        <th>Status</th>
+                        <th width="100">Action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+    const ajaxURL = "<?php echo Route::is('admin.backend.booking_request.trashed' ? 'booking-request/trashed/view' : 'booking_request') ?>";
+    $('table#booking_requests_table').DataTable({
+        dom: 'Blfrtip',
+        language: {processing: "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span> Loading Data..."},
+        processing: true,
+        serverSide: true,
+        ajax: {url: ajaxURL},
+        aLengthMenu: [[25, 50, 100, 1000, -1], [25, 50, 100, 1000, "All"]],
+        buttons: ['excel', 'pdf', 'print'],
+        columns: [
+            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+            {data: 'name', name: 'name'},
+            {data: 'email', name: 'email'},
+            {data: 'phone_no', name: 'phone_no'},
+            {data: 'service_name', name: 'service_name'},
+            {data: 'start_date', name: 'start_date'},
+            {data: 'status', name: 'status'},
+            {data: 'action', name: 'action'}
+        ]
+    });
+    </script>
+@endsection
