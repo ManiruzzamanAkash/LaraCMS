724975ea1a86fed9cfb1afea273874eb36468f17
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-21 15:09:24 +0600

    benefits page content updated

--- Modules/ThemeBusiness/Resources/views/frontend/pages/partials/our-benefit.blade.php
+++ Modules/ThemeBusiness/Resources/views/frontend/pages/partials/our-benefit.blade.php
@@ -10,9 +10,9 @@
                 </div>
                 <h3 class="text-color-secondary font-weight-bold text-transform-none text-8 mb-3 pb-1 appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1200" style="animation-delay: 1200ms;">Our Benefits</h3>
                 <p class="mb-4 appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="1400" style="animation-delay: 1400ms;">
-                    Cras a elit sit amet leo accumsan volutpat. Suspendisse
-                    hendrerit vehicula leo, vel efficitur felis ultrices non. Integer aliquet ullamcorper dolor,
-                    quis sollicitudin. Integer aliquet ullamcorper dolor, quis sollicitudin.
+                    We offer easiest solutions for computer hardware related work and so clients get tense free...
+                    <br>
+                    We provide different solutions to make our client life easier...
                 </p>
             </div>
         </div>
@@ -23,17 +23,15 @@
                         <div class="card-header" id="collapse1HeadingOne">
                             <h4 class="card-title m-0">
                                 <a class="accordion-toggle text-color-dark font-weight-bold collapsed" data-bs-toggle="collapse" data-bs-target="#collapse1One" aria-expanded="false" aria-controls="collapse1One">
-                                    1 - Why Choose Business Website Services?
+                                    1 - Why Choose {{ config('app.name') }}
                                 </a>
                             </h4>
                         </div>
                         <div id="collapse1One" class="collapse" aria-labelledby="collapse1HeadingOne" data-bs-parent="#accordion1">
                             <div class="card-body">
-                                <p class="mb-0">Donec tellus massa, tristique sit amet condim vel, facilisis quis
-                                    sapien. Praesent id enim sit amet odio vulputate eleifend in in tortor. Donec
-                                    tellus massa, tristique sit amet condim vel, facilisis quis sapien. Praesent id
-                                    enim sit amet odio vulputate eleifend in in tortor. Donec tellus massa,
-                                    tristique sit amet condim vel, facilisis quis sapien.</p>
+                                <p class="mb-0">
+                                    We offer easiest solutions for computer hardware related work and so clients get tense free...
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -41,71 +39,15 @@
                         <div class="card-header" id="collapse1HeadingTwo">
                             <h4 class="card-title m-0">
                                 <a class="accordion-toggle text-color-dark font-weight-bold collapsed" data-bs-toggle="collapse" data-bs-target="#collapse1Two" aria-expanded="false" aria-controls="collapse1Two">
-                                    2 - Cras a elit sit amet leo accumsan?
+                                    2 - When we'll take payment ?
                                 </a>
                             </h4>
                         </div>
                         <div id="collapse1Two" class="collapse" aria-labelledby="collapse1HeadingTwo" data-bs-parent="#accordion1">
                             <div class="card-body">
-                                <p class="mb-0">Donec tellus massa, tristique sit amet condim vel, facilisis quis
-                                    sapien. Praesent id enim sit amet odio vulputate eleifend in in tortor. Donec
-                                    tellus massa, tristique sit amet condim vel, facilisis quis sapien. Praesent id
-                                    enim sit amet odio vulputate eleifend in in tortor. Donec tellus massa,
-                                    tristique sit amet condim vel, facilisis quis sapien.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card card-default">
-                        <div class="card-header" id="collapse1HeadingThree">
-                            <h4 class="card-title m-0">
-                                <a class="accordion-toggle text-color-dark font-weight-bold collapsed" data-bs-toggle="collapse" data-bs-target="#collapse1Three" aria-expanded="false" aria-controls="collapse1Three">
-                                    3 - Hel officitur felis ultricis nan?
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse1Three" class="collapse" aria-labelledby="collapse1HeadingThree" data-bs-parent="#accordion1">
-                            <div class="card-body">
-                                <p class="mb-0">Donec tellus massa, tristique sit amet condim vel, facilisis quis
-                                    sapien. Praesent id enim sit amet odio vulputate eleifend in in tortor. Donec
-                                    tellus massa, tristique sit amet condim vel, facilisis quis sapien. Praesent id
-                                    enim sit amet odio vulputate eleifend in in tortor. Donec tellus massa,
-                                    tristique sit amet condim vel, facilisis quis sapien.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card card-default">
-                        <div class="card-header" id="collapse1HeadingFour">
-                            <h4 class="card-title m-0">
-                                <a class="accordion-toggle text-color-dark font-weight-bold collapsed" data-bs-toggle="collapse" data-bs-target="#collapse1Four" aria-expanded="false" aria-controls="collapse1Four">
-                                    4 - Wuspaisse hendreirit vehicula leo?
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse1Four" class="collapse" aria-labelledby="collapse1HeadingFour" data-bs-parent="#accordion1">
-                            <div class="card-body">
-                                <p class="mb-0">Donec tellus massa, tristique sit amet condim vel, facilisis quis
-                                    sapien. Praesent id enim sit amet odio vulputate eleifend in in tortor. Donec
-                                    tellus massa, tristique sit amet condim vel, facilisis quis sapien. Praesent id
-                                    enim sit amet odio vulputate eleifend in in tortor. Donec tellus massa,
-                                    tristique sit amet condim vel, facilisis quis sapien.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card card-default">
-                        <div class="card-header" id="collapse1HeadingFive">
-                            <h4 class="card-title m-0">
-                                <a class="accordion-toggle text-color-dark font-weight-bold collapsed" data-bs-toggle="collapse" data-bs-target="#collapse1Five" aria-expanded="false" aria-controls="collapse1Five">
-                                    5 - Lintegers aliquet ullamcorper dollor, quis sollic tudin?
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse1Five" class="collapse" aria-labelledby="collapse1HeadingFive" data-bs-parent="#accordion1">
-                            <div class="card-body">
-                                <p class="mb-0">Donec tellus massa, tristique sit amet condim vel, facilisis quis
-                                    sapien. Praesent id enim sit amet odio vulputate eleifend in in tortor. Donec
-                                    tellus massa, tristique sit amet condim vel, facilisis quis sapien. Praesent id
-                                    enim sit amet odio vulputate eleifend in in tortor. Donec tellus massa,
-                                    tristique sit amet condim vel, facilisis quis sapien.</p>
+                                <p class="mb-0">
+                                    We offer easiest solutions for computer hardware related work and so clients get tense free...
+                                </p>
                             </div>
                         </div>
                     </div>
