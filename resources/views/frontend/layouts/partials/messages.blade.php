6f74bdb2805a4d36764cc1ee5abe863943145e7e
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-21 06:36:14 +0600

    Message compatible with bootstrap 5

--- resources/views/frontend/layouts/partials/messages.blade.php
+++ resources/views/frontend/layouts/partials/messages.blade.php
@@ -17,14 +17,13 @@
 @endif
 
 @if ($errors->any())
-<div class="alert alert-danger">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">×</span> </button>
-    <h3 class="text-danger"><i class="fa fa-times-circle"></i> Error</h3>
-    <div>
-        @foreach ($errors->all() as $error)
-          <p>{{ $error }}</p>
-        @endforeach
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <h3 class="text-danger"><i class="fa fa-times-circle"></i> Error</h3>
+        <div>
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        </div>
     </div>
-</div>
 @endif
