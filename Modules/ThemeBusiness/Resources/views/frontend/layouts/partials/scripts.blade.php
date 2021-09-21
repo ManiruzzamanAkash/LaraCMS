c1ea71f14b6566cb63a5c353f925d704f4561f5b
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-09-21 15:09:01 +0600

    Commented out un-authenticated services

--- Modules/ThemeBusiness/Resources/views/frontend/layouts/partials/scripts.blade.php
+++ Modules/ThemeBusiness/Resources/views/frontend/layouts/partials/scripts.blade.php
@@ -7,21 +7,21 @@
 <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.easing.min.js')}}"></script>
 <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.cookie.min.js')}}"></script>
 <script src="{{ asset('public/modules/theme-business/js/vendor/bootstrap.bundle.min.js')}}"></script>
-<script src="{{ asset('public/modules/theme-business/js/vendor/jquery.validate.min.js')}}"></script>
-<script src="{{ asset('public/modules/theme-business/js/vendor/jquery.easypiechart.min.js')}}"></script>
-<script src="{{ asset('public/modules/theme-business/js/vendor/jquery.gmap.min.js')}}"></script>
+{{-- <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.validate.min.js')}}"></script> --}}
+{{-- <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.easypiechart.min.js')}}"></script> --}}
+{{-- <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.gmap.min.js')}}"></script> --}}
 <script src="{{ asset('public/modules/theme-business/js/vendor/lazysizes.min.js')}}"></script>
 <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.isotope.min.js')}}"></script>
 <script src="{{ asset('public/modules/theme-business/js/vendor/owl.carousel.min.js')}}"></script>
 <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{ asset('public/modules/theme-business/js/vendor/jquery.vide.min.js')}}"></script>
-<script src="{{ asset('public/modules/theme-business/js/vendor/vivus.min.js')}}"></script>
+{{-- <script src="{{ asset('public/modules/theme-business/js/vendor/jquery.vide.min.js')}}"></script> --}}
+{{-- <script src="{{ asset('public/modules/theme-business/js/vendor/vivus.min.js')}}"></script> --}}
 
 {{-- Parsley Validation JS --}}
 <script src="{{ asset('public/assets/common/vendor/parsley/parsley.min.js')}}"></script>
 
 {{-- Axios JS --}}
-<script src="{{ asset('public/assets/common/vendor/axios/axios.min.js')}}"></script>
+{{-- <script src="{{ asset('public/assets/common/vendor/axios/axios.min.js')}}"></script> --}}
 
 <!-- Theme Base, Components and Settings -->
 <script src="{{ asset('public/modules/theme-business/js/vendor/theme.js')}}"></script>
@@ -31,16 +31,19 @@
 
 
 <!-- Demo -->
-<script src="{{ asset('public/modules/theme-business/js/vendor/demo-cleaning-services.js')}}"></script>
+{{-- <script src="{{ asset('public/modules/theme-business/js/vendor/demo-cleaning-services.js')}}"></script> --}}
 
 <!-- Theme Initialization Files -->
 <script src="{{ asset('public/modules/theme-business/js/vendor/theme.init.js')}}"></script>
-<a class="scroll-to-top hidden-mobile visible" href="#"></a><i class="fas fa-chevron-up"></i></a>
 
 <script defer="" src="{{ asset('public/modules/theme-business/js/vendor/beacon.min.js')}}" data-cf-beacon="{&quot;rayId&quot;:&quot;66c7b7818837d9d4&quot;,&quot;version&quot;:&quot;2021.6.0&quot;,&quot;r&quot;:1,&quot;token&quot;:&quot;03fa3b9eb60b49789931c4694c153f9b&quot;,&quot;si&quot;:10}"></script>
 
 <!-- Noty JS -->
 <script src="{{ asset('public/assets/common/vendor/noty/noty.min.js') }}"></script>
+
+<script>
+    document.body.style.margin = "0px";
+</script>
 
 @include('frontend.layouts.partials.flash-messages')
 
